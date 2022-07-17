aebb259e79a98a6bd9e42d92890a5aa7afbbe549
김영빈 <colock123@gmail.com>
2022-07-17 13:40:28 +0900

    add:field dynamic filtering

--- products/serializers.py
+++ products/serializers.py
@@ -2,6 +2,22 @@ from rest_framework import serializers
 from .models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
+    
+    #원래라면 _category 혹은 category__name과 같은 식으로 해서, 해당 serializer를 활용한 create,update를 사용할 때, category나 origin이라는 이름이 겹쳐져서 발생하는 에러를 고려해야 하겠지만, 이 프로젝트에서는 어차피 Product를 Create,Update할 일이 없음
+    category = serializers.CharField(source='category.name',read_only=True)
+    origin = serializers.CharField(source='origin.name',read_only=True)
+    
+    def __init__(self,*args,**kwargs):
+        fields = kwargs.pop('fields',None)
+        super().__init__(*args,**kwargs)
+        
+        if fields:
+            allowed = set(fields)
+            existing = set(self.fields)
+            for field_name in (existing - allowed):
+
+                self.fields.pop(field_name)
+    
     class Meta:
         model = Product
         fields = '__all__'
--- products/views.py
+++ products/views.py
@@ -1,11 +1,12 @@
-from rest_framework         import generics
-from rest_framework.filters import OrderingFilter
-from .models                import Product
-from .serializers           import ProductSerializer
-from django_filters         import rest_framework as filters
-from core.paginations       import PagePagination
-from core.filters           import ProductListFilter
-from django.shortcuts       import get_object_or_404
+from rest_framework          import generics
+from rest_framework.filters  import OrderingFilter
+from rest_framework.response import Response
+from .models                 import Product
+from .serializers            import ProductSerializer
+from django_filters          import rest_framework as filters
+from core.paginations        import PagePagination
+from core.filters            import ProductListFilter
+from django.shortcuts        import get_object_or_404
 
 # Create your views here.
 
@@ -17,3 +18,19 @@ class ProductListAPIView(generics.ListAPIView):
     queryset         = Product.objects.all()
     ordering         = ['-rating','price']
     ordering_fields  = ['rating','price']
+    
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        #dyanamic field filtering
+        fields     = request.query_params.get('fields')
+        if fields:
+            fields = tuple(fields.split(','))
+        
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True,fields=fields)
+            return self.get_paginated_response(serializer.data)
+        
+        serializer = self.get_serializer(queryset, many=True,fields=fields)
+        return Response(serializer.data)
